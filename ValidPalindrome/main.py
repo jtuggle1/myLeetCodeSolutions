fc821f4fcc016ae34fa9750bb2a942c72d885280
Justin Tuggle <jtuggle1@asu.edu>
2024-08-17 19:50:57 -0700

    Valid Palindrome (first python leetcode)

---
+++ ValidPalindrome/main.py
@@ -0,0 +1,24 @@
+
+def isPalindrome(s):
+    if len(s) == 1:
+        return True
+    r = ""
+    for char in s:
+        if char == " ":
+            continue
+        else:
+            if char.isalpha():
+                r += char.lower()
+            elif char.isdigit():
+                r += char
+    r.lower()
+    rev = r[::-1]
+    return rev == r
+
+
+
+if __name__ == '__main__':
+    s = "gag ::ag"
+    res = isPalindrome(s)
+    print(res)
+
