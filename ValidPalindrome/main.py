daa3281f721a02d11eab54ecaa0e5f0aef045658
dev work <jtuggle1@asu.edu>
2024-08-17 20:24:02 -0700

    Formatting

--- ValidPalindrome/main.py
+++ ValidPalindrome/main.py
@@ -1,24 +1,23 @@
 
 def isPalindrome(s):
-    if len(s) == 1:
-        return True
-    r = ""
-    for char in s:
-        if char == " ":
-            continue
-        else:
-            if char.isalpha():
-                r += char.lower()
-            elif char.isdigit():
-                r += char
-    r.lower()
-    rev = r[::-1]
-    return rev == r
+	if len(s) == 1:
+		return True
+	r = ""
+	for char in s:
+		if char == " ":
+			continue
+		else:
+			if char.isalpha():
+				r += char.lower()
+			elif char.isdigit():
+				r += char
+				r.lower()
+	rev = r[::-1]
+	return rev == r
 
 
 
 if __name__ == '__main__':
-    s = "gag ::ag"
-    res = isPalindrome(s)
-    print(res)
-
+	s = "gag ::a"
+	res = isPalindrome(s)
+	print(res)
